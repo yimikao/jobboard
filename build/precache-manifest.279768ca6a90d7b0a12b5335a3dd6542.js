8eaa28b43440265f9c577b1e2712a3eb4d091a47
yinka <holayinkajr@gmail.com>
2021-07-18 19:39:36 +0100

    commit

---
+++ build/precache-manifest.279768ca6a90d7b0a12b5335a3dd6542.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c754257ef55efdc63141b0da609cbd1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "297708d748c8231bca2f",
+    "url": "/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "e3fdf11fe1942a177acd",
+    "url": "/static/js/2.c4808028.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/static/js/2.c4808028.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "297708d748c8231bca2f",
+    "url": "/static/js/main.ae20ae62.chunk.js"
+  },
+  {
+    "revision": "bf9fda22f05c083606a6",
+    "url": "/static/js/runtime-main.ccf115f7.js"
+  }
+]);
